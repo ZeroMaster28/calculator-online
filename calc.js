568767e497ee6165c74161db08c551944581d1c1
Michael <michalsobieraj966@wp.pl>
2020-08-16 21:53:05 +0200

    code refactor and zero division bugfix

--- calc.js
+++ calc.js
@@ -1,76 +1,69 @@
 // All calculator logic goes here
 
-function Calculator(initialValue)
+function Calculator(symbolsLimit)
 {
-	// limit of the symbols on the screen, 100 symbols initially
-	this.symbolsLimit = 100;
-	// flag indicating that the result can be computed
+	// limit of the symbols on the screen
+	this.symbolsLimit = symbolsLimit;
+	// flag indicating whether two numbers and operaton are present
 	this.canFindResult = false;
-	// flag indicating whether operation symbol is present
-	this.hasOperation = false;
 	// flag indicating whether comma for current number is present
-	this.hasComma = initialValue.includes(".");
-	// current value on the screen
-	this.screenValue = initialValue;
-	// screen's initial value
-	this.initialValue = initialValue;
-	// initialization of method that refreshes the screen after some changes
-	this.refresh = function(){
-		document.getElementById("calc_screen").innerHTML = this.screenValue;
+	this.hasComma = false;
+	// first number
+	this.firstNumber = "";
+	//second number
+	this.secondNumber = "";
+	//operation symbol
+	this.operation = "";
+	// method that sets screen value
+	this.setScreenValue = function(value){
+		document.getElementById("calc_screen").innerHTML = value;
 	}
-	// initial refreshing
-	this.refresh();
+	// setting initial state of the screen
+	this.setScreenValue("0.0");
+	// initialization of the method that refreshes the screen after some changes
+	this.refresh = function(){
+		this.setScreenValue(this.firstNumber + this.operation + this.secondNumber);
+	}
 	// initialization of the method that appends next number symbol to the screen
 	this.putNumber = function(number){
-		if(this.hasOperation){ 
-		// if there's an operation defined we change the state so the computations can be done
-			this.hasOperation = false;
+		if(this.firstNumber.length + this.operation.length + this.secondNumber.length > this.symbolsLimit)
+			return;
+		if(parseInt(this.firstNumber) == 0)
+			this.firstNumber = number + "";
+		else if(this.operation == "")
+			this.firstNumber += number;
+		else if(this.operaton != ""){
+			this.secondNumber += number;
 			this.canFindResult = true;
 		}
-		// changing value of the screen
-		if(parseInt(this.screenValue) == 0)
-			this.screenValue = number + "";
-		else
-			this.screenValue += number;
-		// refresh of the changes
 		this.refresh();
 	}
-	// method that appends operation symbol or comma to the screen string value
+	// method that appends operation symbol to the screen string value
 	this.putSymbol = function(symbol){
-		if(this.screenValue.length > this.symbolsLimit) return;
-		if((!this.hasOperation && !this.canFindResult) || (!this.hasComma && symbol == "."))
-		{
-			this.screenValue += symbol;
-			this.refresh();
-			if(symbol != "."){
-				this.hasOperation = true;
-				this.hasComma = false;
-			}
-			else
-				this.hasComma = true;
+		if(this.operation == ""){
+			this.operation = symbol;
+			this.hasComma = false;
 		}
+		this.refresh();
 	}
 	// initialization of the method that computes results based on the included operation
 	this.findResult = function(){
-		if(this.screenValue.includes("+"))
+		if(this.operation == "+")
 		{
-			var num = this.screenValue.split("+");
-			return parseFloat(num[0]) + parseFloat(num[1]);
+			return parseFloat(this.firstNumber) + parseFloat(this.secondNumber);
 		}
-		else if(this.screenValue.includes("-"))
+		else if(this.operation == "-")
 		{
-			var num = this.screenValue.split("-");
-			return parseFloat(num[0]) - parseFloat(num[1]);
+			return parseFloat(this.firstNumber) - parseFloat(this.secondNumber);
 		}
-		else if(this.screenValue.includes("*"))
+		else if(this.operation == "*")
 		{
-			var num = this.screenValue.split("*");
-			return parseFloat(num[0]) * parseFloat(num[1]);
+			return parseFloat(this.firstNumber) * parseFloat(this.secondNumber);
 		}
-		else if(this.screenValue.includes("/"))
+		else if(this.operation == "/")
 		{
-			var num = this.screenValue.split("/");
-			return parseFloat(num[0]) / parseFloat(num[1]);
+			if(parseInt(this.secondNumber) == 0) return 0;
+			return parseFloat(this.firstNumber) / parseFloat(this.secondNumber);
 		}
 	}
 	// initialzation of the method that performs proper operations based on the screen value
@@ -78,19 +71,26 @@ function Calculator(initialValue)
 		if(this.canFindResult)// showing the result if possible i.e two numbers and operation are present
 		{
 			var result = this.findResult();
-			this.screenValue = result + "";
+			this.firstNumber = result + "";
+			this.hasComma = this.firstNumber.includes(".");
 			
 		}// cleaning the ouput screen otherwise i.e setting the initial state of the screen
-		else
-			this.screenValue = this.initialValue + "";
+		else{
+			this.firstNumber = "0.0";
+			this.hasComma = false;
+		}
+		this.secondNumber = "";
+		this.operation = "";
 		this.canFindResult = false;
-		this.hasOperation = false;
-		this.hasComma = this.screenValue.includes(".");
-		// refreshing the screen
 		this.refresh();
 	}
-	// sets limit of the symbols on the screen
-	this.setLimit = function(limit){
-		this.symbolsLimit = limit
+	// method that appends comma
+	this.putComma = function(){
+		if(this.hasComma || this.firstNumber == "" || this.firstNumber =="0.0") return; //states when comma cannot be put
+		if(this.operation == "")
+			this.firstNumber += ".";
+		else this.secondNumber += ".";
+		this.hasComma = true;
+		this.refresh();
 	}
 }
